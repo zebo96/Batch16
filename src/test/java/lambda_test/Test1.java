c96b29f62d38781f0261df627d28b271abf4f390
zebo <zebo9402@gmail.com>
2023-06-14 22:33:14 -0500

    myFirstCommit

---
+++ src/test/java/lambda_test/Test1.java
@@ -0,0 +1,23 @@
+package lambda_test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Test1 {
+    @Test
+    public void TC_01(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
+        driver.get("https://www.lambdatest.com/selenium-playground");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+    }
+}
